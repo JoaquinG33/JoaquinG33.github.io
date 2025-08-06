45a4879df6a66a601391c59b05b1651c73d63812
Joaquin Guillen <joaquinguillen33@hotmail.com>
2025-08-06 11:45:07 -0300

    agrego animaciones

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const botonNaturaleza = document.getElementById("btnNaturaleza");
-const botonCiudad = document.getElementById("btnCiudad");
-const botonGente = document.getElementById("btnGente");
+const botonNaturaleza = document.getElementById("icoNaturaleza");
+const botonCiudad = document.getElementById("icoCiudad");
+const botonGente = document.getElementById("icoGente");
+const botonComida = document.getElementById("icoComida");
 const fondoGaleria = document.getElementById("galeriaId");
 const fondoContainer = document.getElementById("containerId");
 const titulo = document.getElementById("tituloId");
@@ -12,15 +13,15 @@ let menu = document.querySelector(".menuDesplegableDiv");
 const imgNaturaleza = document.querySelectorAll(".naturaleza");
 const imgCiudad = document.querySelectorAll(".ciudad");
 const imgGente = document.querySelectorAll(".gente");
+const imgComida = document.querySelectorAll(".comida")
 const global = document.querySelectorAll(".global");
 const botones = document.querySelectorAll(".boton");
 
 
-// botonNaturaleza.onclick = fotosNaturaleza;
-// botonCiudad.onclick = fotosCiudad;
-// botonGente.onclick = fotosGente;
-
-
+botonNaturaleza.onclick = fotosNaturaleza;
+botonCiudad.onclick = fotosCiudad;
+botonGente.onclick = fotosGente;
+botonComida.onclick = fotosComida;
 menuClick.onclick = desplegar;
 
 
@@ -28,18 +29,115 @@ menuClick.onclick = desplegar;
 function desplegar(){
 if(menu.classList.toggle("change")){
   //PONERLE OPACITY 1 A LOS BOTONES ACA
+    
     containerIconosMenu.classList.toggle("animacionMenu");
    global.forEach((global) => {
     global.style.opacity = "40%";
   });
 }else{
+  
   containerIconosMenu.classList.remove("animacionMenu");
        global.forEach((global) => {
     global.style.opacity = "100%";
+    global.style.display = "block";
   });
 }
 
  
+}
+
+
+
+function fotosNaturaleza() {
+   animacionGaleria();
+    imgNaturaleza.forEach((imgNaturaleza) => {
+    imgNaturaleza.style.opacity = "100%";
+    imgNaturaleza.style.display = "block";
+  });
+
+  imgCiudad.forEach((imgCiudad) => {
+    imgCiudad.style.display = "none";
+  });
+
+  imgGente.forEach((imgGente) => {
+    imgGente.style.display = "none";
+  });
+
+  imgComida.forEach((imgComida)=> {
+    imgComida.style.display = "none";
+  })
+}
+
+function fotosCiudad() {
+ animacionGaleria();
+ 
+  imgCiudad.forEach((imgCiudad) => {
+    imgCiudad.style.opacity = "100%";
+    imgCiudad.style.display = "block";
+  });
+
+  imgNaturaleza.forEach((imgNaturaleza) => {
+    imgNaturaleza.style.display = "none";
+  });
+
+  imgGente.forEach((imgGente) => {
+    imgGente.style.display = "none";
+  });
+
+  imgComida.forEach((imgComida)=>{
+    imgComida.style.display = "none";
+  })
+}
+
+function fotosGente() {
+  animacionGaleria();
+  
+  imgGente.forEach((imgGente) => {
+    imgGente.style.opacity = "100%"
+    imgGente.style.display = "block";
+  });
+
+  imgCiudad.forEach((imgCiudad) => {
+    imgCiudad.style.display = "none";
+  });
+
+  imgNaturaleza.forEach((imgNaturaleza) => {
+    imgNaturaleza.style.display = "none";
+  });
+
+  imgComida.forEach((imgComida)=>{
+    imgComida.style.display = "none";
+  })
+}
+
+function fotosComida(){
+  animacionGaleria();
+  
+  imgComida.forEach((imgComida) => {
+    imgComida.style.opacity = "100%"
+    imgComida.style.display = "block";
+  });
+
+  imgCiudad.forEach((imgCiudad) => {
+    imgCiudad.style.display = "none";
+  });
+
+  imgNaturaleza.forEach((imgNaturaleza) => {
+    imgNaturaleza.style.display = "none";
+  });
+
+  imgGente.forEach((imgGente)=>{
+    imgGente.style.display = "none";
+  })
+}
+
+function animacionGaleria(){
+  fondoContainer.classList.toggle("animacionGaleria");
+
+  function removerClase(){
+    fondoContainer.classList.remove("animacionGaleria");
+  }
+ setTimeout(removerClase, 750);
 }
 
 
@@ -74,74 +172,3 @@ if(menu.classList.toggle("change")){
 //     imgGente.style.opacity = "100%";
 //   });
 // });
-
-function fotosNaturaleza() {
-  titulo.style.background = "var(--naturaleza)";
-  fondoGaleria.style.borderColor = "var(--naturalezaFondo)";
-  fondoContainer.style.background = "var(--naturalezaFondo)";
-  // iconosFondo.style.background = "var(--naturalezaFondo)";
-
-  botones.forEach((botones) => {
-    botones.style.background = "var(--naturalezaFondo)";
-    botones.style.color = "var(--naturaleza)";
-  });
-
-  imgNaturaleza.forEach((imgNaturaleza) => {
-    imgNaturaleza.style.opacity = "100%";
-  });
-
-  imgCiudad.forEach((imgCiudad) => {
-    imgCiudad.style.opacity = "15%";
-  });
-
-  imgGente.forEach((imgGente) => {
-    imgGente.style.opacity = "15%";
-  });
-}
-
-function fotosCiudad() {
-  titulo.style.background = "var(--ciudad)";
-  fondoGaleria.style.borderColor = "var(--ciudadFondo)";
-  fondoContainer.style.background = "var(--ciudadFondo)";
-  // iconosFondo.style.background = "var(--ciudadFondo)";
-
-  botones.forEach((botones) => {
-    botones.style.background = "var(--ciudadFondo)";
-    botones.style.color = "var(--ciudad)";
-  });
-
-  imgCiudad.forEach((imgCiudad) => {
-    imgCiudad.style.opacity = "100%";
-  });
-
-  imgNaturaleza.forEach((imgNaturaleza) => {
-    imgNaturaleza.style.opacity = "15%";
-  });
-
-  imgGente.forEach((imgGente) => {
-    imgGente.style.opacity = "15%";
-  });
-}
-
-function fotosGente() {
-  titulo.style.background = "var(--gente)";
-  fondoGaleria.style.borderColor = "var(--genteFondo)";
-  fondoContainer.style.background = "var(--genteFondo)";
-
-  botones.forEach((botones) => {
-    botones.style.background = "var(--genteFondo)";
-    botones.style.color = "var(--gente)";
-  });
-
-  imgGente.forEach((imgGente) => {
-    imgGente.style.opacity = "100%";
-  });
-
-  imgCiudad.forEach((imgCiudad) => {
-    imgCiudad.style.opacity = "15%";
-  });
-
-  imgNaturaleza.forEach((imgNaturaleza) => {
-    imgNaturaleza.style.opacity = "15%";
-  });
-}
